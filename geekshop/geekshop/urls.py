3eac68b6db26222ed466a627886e1fa01085b750
usermuser <lwrdrstv@gmail.com>
2019-01-19 01:34:41 +0500

    hw1 done

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mainapp.views as mainapp
 
 urlpatterns = [
+    path('', mainapp.index),
+    path('products/', mainapp.products),
+    path('contact/', mainapp.products),
     path('admin/', admin.site.urls),
 ]
