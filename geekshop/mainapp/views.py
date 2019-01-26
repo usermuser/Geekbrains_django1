2337f62d1816bb706b9fb36eac9172e09c284c43
usermuser <lwrdrstv@gmail.com>
2019-01-26 08:51:15 +0500

    added layout.html, changed templates structure to enable inheritance mechanism

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+# def index(request:HttpRequest):
+#     return render(request, 'mainapp/index.html')
+
 def index(request:HttpRequest):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index_child.html')
 
 def products(request:HttpRequest):
     return render(request, 'mainapp/products.html')
