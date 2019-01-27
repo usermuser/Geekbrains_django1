c1595eaab8da688a99c8e1062fe5690d73ff4f48
usermuser <lwrdrstv@gmail.com>
2019-01-28 01:57:27 +0500

    updated comments in mainapp/urls.py

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -6,8 +6,10 @@ app_name = 'mainapp'
 urlpatterns = [
     # http://127.0.0.1:8000/shop/
     path('',views.categories, name='categories'),
-    # http://127.0.0.1:8000/shop/category/1 or 2 or any number
+
+    # http://127.0.0.1:8000/shop/category/pechenki or chai (any category_slug)
     path('category/<slug:category_slug>/', views.products_list, name='products'),
+
     # http://127.0.0.1:8000/shop/product/1 or 2 or any number
     path('product/<int:product_id>/', views.product_details, name='product_details'),
 
