55703e9673ffac8d8600d6db93d16936fd504a88
usermuser <lwrdrstv@gmail.com>
2019-01-27 23:04:31 +0500

    modified models, also added them to admin interface

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True, unique=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Имя')
+    slug = models.SlugField(max_length=100, db_index=True, unique=True, verbose_name='Ссылка')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='products',
+                                 verbose_name='Категория'
+                                 )
 
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Имя')
+    slug = models.SlugField(max_length=100, db_index=True, verbose_name='Ссылка')
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -24,6 +33,8 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
     def __str__(self):
         return self.name
