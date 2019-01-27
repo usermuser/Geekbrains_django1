9f58fe82e90d71557247334863f269d6a68b6927
usermuser <lwrdrstv@gmail.com>
2019-01-27 22:11:56 +0500

    added media settings and paths

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', mainapp.index, name='main'),
@@ -23,3 +29,9 @@ urlpatterns = [
     path('contacts/', mainapp.contacts, name='contacts'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
