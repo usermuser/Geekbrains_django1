6294410cf082529dd34d53556fac7e8d1c356b7d
usermuser <lwrdrstv@gmail.com>
2019-02-10 14:32:39 +0500

    created forms.py file in authapp

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -5,6 +5,12 @@ def redirect_to_login(request:HttpRequest   ):
     return HttpResponseRedirect('/auth/login')
 
 def login(request:HttpRequest):
+    title = 'войти на сайт'
+
+    # 1. Создать форму для заполнения
+    # 2. Проверить данные из запроса
+    # 3. Выполнить аутентификацию
+
     return HttpResponse('auth/login')
 
 def logout(request:HttpRequest):
