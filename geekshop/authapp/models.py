669f615a752d8cff715c9ae864aada96faee6a5d
usermuser <lwrdrstv@gmail.com>
2019-02-10 18:33:53 +0500

    added image field for CustomUser() class

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+
     age = models.PositiveIntegerField(verbose_name='возраст')
+    avatar = models.ImageField(upload_to='users_avatars/%Y/%m/%d',
+                               verbose_name='аватар',
+                               blank=True)
