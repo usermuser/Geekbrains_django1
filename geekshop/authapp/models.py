b18f37ef1e78e72ecfa77cf4df5ca1c0cc1c736e
usermuser <lwrdrstv@gmail.com>
2019-03-03 22:03:34 +0500

    changed custom user class to ShopUser

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
+class ShopUser(AbstractUser):
 
     age = models.PositiveIntegerField(verbose_name='возраст', default=0)
-    avatar = models.ImageField(upload_to='users_avatars/%Y/%m/%d',
+    avatar = models.ImageField(upload_to='users_avatars',
                                verbose_name='аватар',
                                blank=True)
