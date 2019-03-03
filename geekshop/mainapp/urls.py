83beacc3d8d77b3341dc6c8a29ff33f553029f68
usermuser <lwrdrstv@gmail.com>
2019-03-04 00:17:40 +0500

    img path for same products is correct, the problem is in seed_db (fix script for filling db with images)

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     # http://127.0.0.1:8000/products/1
     path('<int:product_id>/', views.products_list, name='products'),
 
-    # http://127.0.0.1:8000/shop/product/1 or 2 or any number
     # path('product/<int:product_id>/', views.product_details, name='product_details'),
 
 ]
